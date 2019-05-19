91566cc055a26791f37b4bf2838ba7b434873e5b
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-05-19 16:45:35 +0100

    modify sign in form

---
+++ UI/js/app.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+// const hideSigninForm = document.querySelector('#signin-form');
+// const showSignupForm = document.querySelector('#signup-form');
+// // const form = document.querySelector('.form');
+// const toggleForm = (event) => {
+//     if (event.target.id === 'signin-form') {
+//         hideSigninForm.classList.remove('signup-form');
+//         // form.classList.remove('signup-form');
+//         showSignupForm.classList.add('signup-form')
+//     } else {
+//         hideSigninForm.classList.add('signup-form');
+//         // form.classList.remove('signup-form');
+//         showSignupForm.classList.remove('signup-form')
+//     }
+
+// }
+
+// hideSigninForm.addEventListener('click', toggleForm);
+// // form.addEventListener('click', toggleForm);
+// showSignupForm.addEventListener('click', toggleForm);
+
+
+
