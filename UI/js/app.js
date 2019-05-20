ca77e111f38d061d5d802ffd08abeccf937a5f94
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-05-20 09:38:55 +0100

    add function to switch form

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,12 +1,26 @@
-
-let signIn = '';
 window.addEventListener('load', () => {
-  const signUp = document.querySelector('#register');
-  signIn =document.querySelector('#login');
-  
-  // sign up is display on page load
-  const alternateForm = document.querySelectorAll('.alternate-form');
-  alternateForm[0].style.display = 'block';
-  alternateForm[0].querySelector('form input').focus();
+    // sign up is display on page load
+    const alternateForm = document.querySelectorAll('.alternate-form');
+    alternateForm[0].style.display = 'block';
+
+    const signUp = document.querySelector('#register');
+    const signIn = document.querySelector('#login');
+
+    // function to fire event onclick
+    const switchFormOnClick = event => {
+        if (event.target.id === 'login') {
+            alternateForm[0].style.display = "none";
+            alternateForm[1].style.display = "block";
+        } else {
+            alternateForm[1].style.display = "none";
+            alternateForm[0].style.display = "block";
+
+        }
+    }
+
+    // display specify form on click
+    signUp.addEventListener('click', switchFormOnClick);
+    signIn.addEventListener('click', switchFormOnClick);
+
 
 })
