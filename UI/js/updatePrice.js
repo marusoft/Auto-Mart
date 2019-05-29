3119ab856230e860d8fa766d1d739f119e70384a
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-05-29 18:00:32 +0100

    create to close modal on outerside click

--- UI/js/updatePrice.js
+++ UI/js/updatePrice.js
@@ -13,8 +13,17 @@ const closeUpdateModalOnclick = () => {
   updatePriceModal.style.display = "none";
 };
 
+// function to close update price modal on outer area click
+const closeModalOnClickOuterArea = event => {
+  event.target == updatePriceModal
+    ? (updatePriceModal.style.display = "none")
+    : (updatePriceModal.style.display = false);
+};
+
 // Event handlers
 // on click,it opens the update price modal
 updatePriceBtn.onclick = openUpdateModalOnclick;
 // on click,it close the update price modal
 closeUpdateModal.onclick = closeUpdateModalOnclick;
+// on click,it close the update price modal
+window.onclick = closeModalOnClickOuterArea;
