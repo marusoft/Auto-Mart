0c8f4ef9849f99f2d863583044f6274762ef9bba
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-02 18:11:30 +0100

    add js function toggle menu onclick

---
+++ UI/js/navbar.js
@@ -0,0 +1,10 @@
+const hambugerContain = document.querySelector("#hambuger-contain");
+const navList = document.querySelector(".nav-list");
+
+const toggleBar = () => {
+  hambugerContain.classList.toggle("toggler");
+  navList.classList.toggle("toggler");
+};
+
+hambugerContain.addEventListener("click", toggleBar);
+navList.addEventListener("click", toggleBar);
