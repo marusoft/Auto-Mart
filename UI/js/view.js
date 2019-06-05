b954f6c6ee69fbe3da53c3ab88743edfd94e0d1d
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-05 14:54:47 +0100

    create a function to open modal on click buuton view

--- UI/js/view.js
+++ UI/js/view.js
@@ -1,3 +1,9 @@
 const modaView = document.querySelector('#view-modal');
 const viewBtn = document.querySelector('#btn-view1');
 const CloseBtn = document.querySelector('#close');
+
+// create a function to open modal
+const openModal =() => {
+  modalView.style.display = 'block';
+}
+
