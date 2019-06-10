2334cd1069b0cba96a500de33d779ce9b589965d
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-10 10:29:41 +0100

    add loginusers method and controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -28,5 +28,26 @@ class Users {
       message: 'Successfully created',
     });
   }
+
+  /**
+   * @static
+   * @returns {object} loginUsers
+   * @params {*} req
+   * @params {*} res
+   */
+  static loginUsers(req, res) {
+    const { email, password } = req.body;
+    const foundUserEmail = users.find(user => user.email === email);
+    const foundUserPassword = users.find(pass => pass.password === password);
+    if (foundUserEmail && foundUserPassword) {
+      res.status(200).json({
+        message: 'You signed in ...',
+      });
+    } else {
+      res.status(401).json({
+        error: 'You need to register or supply the correct input ...',
+      });
+    }
+  }
 }
 export default Users;
