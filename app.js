cd8a029d147a0d4d57f294b8b248ffc9c2a8b553
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-10 10:24:27 +0100

    install validatorjs and add endpoint for signup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import Users from './server/controllers/usersController';
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 1440;
@@ -8,9 +9,11 @@ const port = parseInt(process.env.PORT, 10) || 1440;
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("*", (req, res) => res.status(200).send({
+app.post('/api/v1/auth/signup', Users.createUsers);
+
+app.get('*', (req, res) => res.status(200).send({
   message: 'It all start from localhost!!!',
 }));
 
