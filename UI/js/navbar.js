8e2aab7c614be9a40cbe1dce339751d94b97accb
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2019-06-13 11:50:36 +0100

    remove conflict from the code

--- UI/js/navbar.js
+++ UI/js/navbar.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 const hambugerContain = document.querySelector("#hambuger-contain");
 const navList = document.querySelector(".nav-list");
 
@@ -10,15 +9,4 @@ const toggleBar = () => {
 
 hambugerContain.addEventListener("click", toggleBar);
 navList.addEventListener("click", toggleBar);
-=======
-const hambugerContain  = document.querySelector('#hambuger-contain');
-const navList = document.querySelector('.nav-list');
 
-const toggleBar= () => {
-  hambugerContain.classList.toggle('toggler');
-  navList.classList.toggle('toggler')
-}
-
-hambugerContain.addEventListener('click', toggleBar);
-navList.addEventListener('click', toggleBar);
->>>>>>> 6a80366861a911cce979176fa4d9eaf6a4246c95
