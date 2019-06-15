a0818ffc7be79b9680784dd9ac47ec5eea758601
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-15 13:08:53 +0100

    create route for view all cars sold or unsold

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -5,5 +5,6 @@ import CarsValidation from '../middlewares/carsValidation';
 const carRouter = express.Router();
 
 carRouter.post('/car', CarsValidation.validateCarsDetails, Cars.createCarSaleAD);
+carRouter.get('/car', Cars.ViewAllPostedAD);
 
 export default carRouter;
