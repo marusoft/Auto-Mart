d24dfe691c4b5fcead4c51a5176878224ba74e99
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-15 18:46:25 +0100

    rename index.js, add another controller ViewAllPostedAD

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import Cars from '../controllers/carControllers';
 import CarsValidation from '../middlewares/carsValidation';
 
+
 const carRouter = express.Router();
 
 carRouter.post('/car', CarsValidation.validateCarsDetails, Cars.createCarSaleAD);
