82ccb1def2c5b908ceccebf18d624db8a90e489f
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-20 11:58:36 +0100

    create endpoint and controller for view all unsold cars

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -7,10 +7,11 @@ import CarsValidation from '../middlewares/carsValidation';
 const carRouter = express.Router();
 
 carRouter.post('/car', CarsValidation.validateCarsDetails, Cars.createCarSaleAD);
-carRouter.get('/car', Cars.ViewAllPostedAD);
 carRouter.get('/car/:id', CarsValidation.validateSpecifyCar, Cars.ViewASpecificCar);
 carRouter.delete('/car/:id', CarsValidation.validateSpecifyCar, Cars.deleteASpecificCarAD);
 carRouter.patch('/car/:id/status', Cars.updateCarStatus);
 carRouter.patch('/car/:id/price', Cars.updateCarPrice);
+carRouter.get('/car', Cars.ViewAllPostedADCar);
+
 
 export default carRouter;
