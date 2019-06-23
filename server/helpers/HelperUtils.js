4ab91fc5ad63342e7978f65642813d31e03c0b15
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2019-06-23 19:52:30 +0100

    [feature #166840200 modify gitignore]

--- server/helpers/HelperUtils.js
+++ server/helpers/HelperUtils.js
@@ -1,6 +1,7 @@
+import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
-import dotenv from 'dotenv';
+
 
 dotenv.config();
 const secretKey = process.env.SECRET_KEY;
