5688fe7d0b019b9b61d96ca658078787f7d2c43a
marusoft <alimi.kmaruf@gmail.com>
2019-12-03 13:44:28 +0100

    update

--- UI/js/updatePrice.js
+++ UI/js/updatePrice.js
@@ -1,22 +1,22 @@
 // variable to target modal element,0pen modal button and close button
-const updatePriceModal = document.querySelector("#update-price-modal");
-const updatePriceBtn = document.querySelector("#update-price-btn");
-const closeUpdateModal = document.querySelector(".close-update-modal");
+const updatePriceModal = document.querySelector('#update-price-modal');
+const updatePriceBtn = document.querySelector('#update-price-btn');
+const closeUpdateModal = document.querySelector('.close-update-modal');
 
 // function to open update price modal
 const openUpdateModalOnclick = () => {
-  updatePriceModal.style.display = "block";
+  updatePriceModal.style.display = 'block';
 };
 
 // function to close update price modal
 const closeUpdateModalOnclick = () => {
-  updatePriceModal.style.display = "none";
+  updatePriceModal.style.display = 'none';
 };
 
 // function to close update price modal on outer area click
-const closeModalOnClickOuterArea = event => {
+const closeModalOnClickOuterArea = (event) => {
   event.target == updatePriceModal
-    ? (updatePriceModal.style.display = "none")
+    ? (updatePriceModal.style.display = 'none')
     : (updatePriceModal.style.display = false);
 };
 
